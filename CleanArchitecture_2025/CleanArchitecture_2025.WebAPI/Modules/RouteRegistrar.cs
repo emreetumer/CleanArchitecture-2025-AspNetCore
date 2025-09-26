efa4771341433eea4d698dc9c2b1134b078ce8e8
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 19:02:17 +0300

    Web API  katmanı yapısı oluşturuldu.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.WebAPI/Modules/RouteRegistrar.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture_2025.WebAPI.Modules;
+
+public static class RouteRegistrar
+{
+    public static void RegisterRoute(this IEndpointRouteBuilder app)
+    {
+        app.RegisterEmployeeRoutes();
+    }
+}
---
+++ CleanArchitecture_2025/CleanArchitecture_2025.WebAPI/Program.cs
@@ -0,0 +1,56 @@
+using CleanArchitecture_2025.Application;
+using CleanArchitecture_2025.Infrastructure;
+using CleanArchitecture_2025.WebAPI.Controllers;
+using CleanArchitecture_2025.WebAPI.Modules;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.RateLimiting;
+using Scalar.AspNetCore;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers().AddOData(options =>
+{
+    options
+    .Select()
+    .Filter()
+    .Count()
+    .Expand()
+    .OrderBy()
+    .SetMaxTop(null)
+    .AddRouteComponents("odata", AppODataController.GetEdmModel());
+});
+builder.Services.AddRateLimiter(x =>
+x.AddFixedWindowLimiter("fixed", cfg =>
+{
+    cfg.QueueLimit = 100;
+    cfg.Window = TimeSpan.FromSeconds(1);
+    cfg.PermitLimit = 100;
+    cfg.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+}));
+
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+
+app.MapDefaultEndpoints();
+
+app.UseCors(x => x
+.AllowAnyHeader()
+.AllowCredentials()
+.AllowAnyMethod()
+.SetIsOriginAllowed(t => true));
+
+app.RegisterRoute();
+
+app.MapControllers().RequireRateLimiting("fixed");
+
+app.Run();
