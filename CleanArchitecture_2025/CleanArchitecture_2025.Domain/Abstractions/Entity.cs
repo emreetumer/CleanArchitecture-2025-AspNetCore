ac2e086b146238e4c3f7500fd24deb8eb7410061
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 15:05:29 +0300

    Infrastructure katmanı işlemleri yapıldı.

--- CleanArchitecture_2025/CleanArchitecture_2025.Domain/Abstractions/Entity.cs
+++ CleanArchitecture_2025/CleanArchitecture_2025.Domain/Abstractions/Entity.cs
@@ -6,8 +6,8 @@ public abstract class Entity
         Id = Guid.NewGuid();
     }
     public Guid Id { get; set; }
-    public DateTime CreateAt { get; set; }
-    public DateTime? UpdateAt { get; set; }
+    public DateTimeOffset CreateAt { get; set; }
+    public DateTimeOffset? UpdateAt { get; set; }
     public bool IsDeleted { get; set; }
-    public DateTime? DeleteAt { get; set; }
+    public DateTimeOffset? DeleteAt { get; set; }
 }
