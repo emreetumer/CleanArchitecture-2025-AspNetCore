7d927a8575c04ec66b052b4c1542c196eb10a610
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 19:02:17 +0300

    Web API  katmanı yapısı oluşturuldu.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.WebAPI/Controllers/AppODataController.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchitecture_2025.Application.Employees;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace CleanArchitecture_2025.WebAPI.Controllers;
+
+[Route("odata")]
+[ApiController]
+[EnableQuery]
+public class AppODataController : ODataController
+{
+    private readonly ISender _sender;
+
+    public AppODataController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public static IEdmModel GetEdmModel()
+    {
+        ODataConventionModelBuilder builder = new();
+        builder.EnableLowerCamelCase();
+        builder.EntitySet<EmployeeGetAllQueryResponse>("employees");
+        return builder.GetEdmModel();
+    }
+
+    [HttpGet("employees")]
+    //public async Task<IActionResult> GetAllEmployees(CancellationToken cancellationToken)
+    public async Task<IQueryable<EmployeeGetAllQueryResponse>> GetAllEmployees(CancellationToken cancellationToken)
+    {
+        var response = await _sender.Send(new EmployeeGetAllQuery(), cancellationToken);
+        //return Ok(response);
+        return response;
+    }
+}
