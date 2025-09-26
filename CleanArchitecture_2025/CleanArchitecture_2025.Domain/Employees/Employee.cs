39b93c6564a280d585c22023644ceb8690548663
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 08:16:00 +0300

    Domain katmanı işlemleri yapıldı.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Domain/Employees/Employee.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture_2025.Domain.Abstractions;
+
+namespace CleanArchitecture_2025.Domain.Employees;
+public sealed class Employee : Entity
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string FullName => string.Join(" ", FirstName, LastName);
+    public DateOnly BirthOfDate { get; set; }
+    public decimal Salary { get; set; }
+    public PersonelInformation PersonelInformation { get; set; } = default!;
+    public Address? Address { get; set; }
+}
