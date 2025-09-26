a3cc58bae557191b39eda27fdfe30601cdceb8d0
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 10:06:51 +0300

    Application katmanı işlemleri yapıldı.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Application/Employees/EmployeeCreateCommand.cs
@@ -0,0 +1,55 @@
+﻿using CleanArchitecture_2025.Domain.Employees;
+using FluentValidation;
+using GenericRepository;
+using Mapster;
+using MediatR;
+using TS.Result;
+
+namespace CleanArchitecture_2025.Application.Employees;
+public sealed record EmployeeCreateCommand(
+    string FirstName,
+    string LastName,
+    DateOnly BirthOfDate,
+    decimal Salary,
+    PersonelInformation PersonelInformation,
+    Address? Address) : IRequest<Result<string>>;
+
+public sealed class EmployeeCreateCommandValidatior : AbstractValidator<EmployeeCreateCommand>
+{
+    public EmployeeCreateCommandValidatior()
+    {
+        RuleFor(x => x.FirstName).MinimumLength(3).WithMessage("Ad alanı en az 3 karakter olmalıdır.");
+        RuleFor(x => x.LastName).MinimumLength(3).WithMessage("Soyad alanı ne az 3 karakter olamlıdır");
+        RuleFor(x => x.PersonelInformation.TCNo)
+            .MinimumLength(11).WithMessage("Geçerli bir TC numarası yazın")
+            .MaximumLength(11).WithMessage("Geçerli bir TC numarası yazın");
+    }
+}
+
+// Handler metodu başka bir katmandan erişilemesi lazım bu sebeple internal yaptık.
+internal sealed class EmployeeCreateCommandHandler : IRequestHandler<EmployeeCreateCommand, Result<string>>
+{
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    public EmployeeCreateCommandHandler(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork)
+    {
+        _employeeRepository = employeeRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<string>> Handle(EmployeeCreateCommand request, CancellationToken cancellationToken)
+    {
+        var isEmployeeExists = await _employeeRepository.AnyAsync(p => p.PersonelInformation.TCNo == request.PersonelInformation.TCNo, cancellationToken);
+        if (isEmployeeExists)
+        {
+            return Result<string>.Failure("Bu TC numarası daha önce kaydedilmiş");
+        }
+
+        Employee employee = request.Adapt<Employee>();
+
+        _employeeRepository.Add(employee);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Personel kaydı başarıyla tamamlandı.";
+    }
+}
---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Application/Employees/EmployeeGetAllQuery.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchitecture_2025.Domain.Abstractions;
+using CleanArchitecture_2025.Domain.Employees;
+using MediatR;
+
+namespace CleanArchitecture_2025.Application.Employees;
+public sealed record EmployeeGetAllQuery() : IRequest<IQueryable<EmployeeGetAllQueryResponse>>;
+
+public sealed class EmployeeGetAllQueryResponse : EntityDto
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public DateOnly BirthOfDate { get; set; }
+    public decimal Salary { get; set; }
+    public string TCNo { get; set; } = default!;
+}
+
+internal sealed class EmployeeGetAllQueryHandler : IRequestHandler<EmployeeGetAllQuery, IQueryable<EmployeeGetAllQueryResponse>>
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeeGetAllQueryHandler(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public Task<IQueryable<EmployeeGetAllQueryResponse>> Handle(EmployeeGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var response = _employeeRepository.GetAll()
+            .Select(s => new EmployeeGetAllQueryResponse
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                BirthOfDate = s.BirthOfDate,
+                Salary = s.Salary,
+                TCNo = s.PersonelInformation.TCNo,
+                UpdateAt = s.UpdateAt,
+                CreateAt = s.CreateAt,
+                DeleteAt = s.DeleteAt,
+                IsDeleted = s.IsDeleted,
+            })
+            .AsQueryable();
+
+        return Task.FromResult(response);
+    }
+}
