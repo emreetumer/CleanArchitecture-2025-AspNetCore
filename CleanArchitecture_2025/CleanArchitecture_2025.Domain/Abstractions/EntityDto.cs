b5435728a88d53851f16d1464dc624df57bb2095
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 10:06:51 +0300

    Application katmanı işlemleri yapıldı.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Domain/Abstractions/EntityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture_2025.Domain.Abstractions;
+public abstract class EntityDto
+{
+    public Guid Id { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime? UpdateAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeleteAt { get; set; }
+}
