4ffff5144e8a8c62034bb8ab70d72f245b268fda
Emre Tümer <emreetumer0@gmail.com>
2025-09-26 15:05:29 +0300

    Infrastructure katmanı işlemleri yapıldı.

---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Infrastructure/InfrastructureRegistrar.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture_2025.Infrastructure.Context;
+using GenericRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace CleanArchitecture_2025.Infrastructure;
+public static class InfrastructureRegistrar
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            string connectionString = configuration.GetConnectionString("SqlServer")!;
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<ApplicationDbContext>());
+
+        // services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        // Bunun yerine scrutor kütüphanesini kullanabiliriz.
+
+        services.Scan(options => options
+        .FromAssemblies(typeof(InfrastructureRegistrar).Assembly)
+        .AddClasses(publicOnly: false)
+        .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+        .AsImplementedInterfaces()
+        .WithScopedLifetime()
+        );
+        // Bu kod servis ve repository için otomatik dependency injection yapar.
+
+
+
+        return services;
+    }
+}
---
+++ CleanArchitecture_2025/CleanArchitecture_2025.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture_2025.Domain.Employees;
+using CleanArchitecture_2025.Infrastructure.Context;
+using GenericRepository;
+
+namespace CleanArchitecture_2025.Infrastructure.Repositories;
+internal class EmployeeRepository : Repository<Employee, ApplicationDbContext>, IEmployeeRepository
+{
+    public EmployeeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
