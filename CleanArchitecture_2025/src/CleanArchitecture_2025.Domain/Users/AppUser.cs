07edfb4f5179a6c20f7bec367a5f89d95993092c
Emre Tümer <emreetumer0@gmail.com>
2025-09-29 07:38:12 +0300

    Identity kütüphanesi eklendi, User class'ı yazıldı, configuration yapıldı.

--- CleanArchitecture_2025/src/CleanArchitecture_2025.Domain/Users/AppUser.cs
+++ CleanArchitecture_2025/src/CleanArchitecture_2025.Domain/Users/AppUser.cs
@@ -1,11 +1,15 @@
-﻿namespace CleanArchitecture_2025.Domain.Abstractions;
-public abstract class Entity
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitecture_2025.Domain.Users;
+public sealed class AppUser : IdentityUser<Guid>
 {
-    public Entity()
+    public AppUser()
     {
         Id = Guid.CreateVersion7();
     }
-    public Guid Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string FullName => $"{FirstName} {LastName}"; //computed property
 
     #region Audit Log
     public DateTimeOffset CreateAt { get; set; }
---
+++ CleanArchitecture_2025/src/CleanArchitecture_2025.Infrastructure/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture_2025.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture_2025.Infrastructure.Configurations;
+public sealed class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.HasIndex(p => p.UserName).IsUnique();
+        builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
+        builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+        builder.Property(p => p.UserName).HasColumnType("varchar(15)");
+        builder.Property(p => p.Email).HasColumnType("nvarchar(MAX)");
+    }
+}
